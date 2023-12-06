1e2527bc09a6606db40da498149fc8320ee76e30
apatir <153133974+apatir@users.noreply.github.com>
2023-12-06 22:51:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ $(document).ready(function() {
         e.preventDefault();
         var radioValue = $("input[name='eldontendo']:checked").val();
         var attendedEvents = $('#megjelent').val();
-
-        // Calculate the recruitment probability based on radio button value and attended events
         var recruitmentProbability = 0;
 
         if (radioValue === 'igen') {
-            recruitmentProbability += 40; // Set 40% for "igen"
+            recruitmentProbability += 40;
         }
 
-        // Allocate the remaining percentage to attended events
         var remainingPercentage = 100 - recruitmentProbability;
         recruitmentProbability += (attendedEvents / 10) * remainingPercentage;
 
-        // Display the result on the page
         $('#recruitmentResult').text('Felvételi esély: ' + recruitmentProbability.toFixed(2) + '%');
     });
 });
